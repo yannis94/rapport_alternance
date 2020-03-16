5cbbf728b04c2c67c03fe58d6e65a678db15e604
yannis94 <bengueci.yannis@gmail.com>
2020-03-16 18:00:48 +0100

    Template original + script menu

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
-console.log("hello friend")
+(function(){
+    let pages_url = ["/index.html", "/entreprise.html", "/aboutme.html", "/missions.html", "/bilan.html"];
+    let actual_page = document.location.pathname;
+    let menu_div = document.querySelector("#menu");
+    
+    let pages_infos = {
+        "/index.html": {
+            "nom": "Home",
+            "active": false
+        },
+        "/entreprise.html": {
+            "nom": "Entreprise",
+            "active": false
+        },
+        "/aboutme.html": {
+            "nom": "About me",
+            "active": false
+        },
+        "/missions.html": {
+            "nom": "Mes missions",
+            "active": false
+        },
+        "/bilan.html": {
+            "nom": "Bilan",
+            "active": false
+        }
+    }
+    
+    pages_infos[actual_page].active = true;
+    
+    
+    let menu_img = document.createElement("img");
+    let menu_links = document.createElement("div");
+    menu_img.id = "menu_logo";
+    menu_img.src = "#"; //! changer path
+    menu_links.id = "menu_link";
+    
+    menu_div.appendChild(menu_img);
+    menu_div.appendChild(menu_links);
+    
+    
+    pages_url.forEach(path => {
+        let link_page = document.createElement("a");
+        link_page.href = path;
+        menu_links.appendChild(link_page);
+        link_page.innerText = `${pages_infos[path].nom}`;
+    
+        if (pages_infos[path].active) {
+            link_page.classList = "current_page";
+        }
+    });
+    
+})()
