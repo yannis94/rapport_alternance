b98c2ba2f5f3da157cac8fbdbf5f55542e92f881
yannis94 <33447485+yannis94@users.noreply.github.com>
2020-06-24 12:01:45 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
 	define("DB_HOST", "localhost");
-	define("DB_USER", "ghost-2306");
-	define("DB_PASS", "root");
+	define("DB_USER", "");
+	define("DB_PASS", "");
 	define("DB_BASE", "rapport_alternance");
